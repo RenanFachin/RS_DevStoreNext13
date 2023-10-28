86255cb9dd15233f93a7358f8202b11703609f69
Renan <renan.fachin@hotmail.com>
2023-10-27 23:26:46 -0300

    feat: Fetch de produtos na página home com server side

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,11 +1,14 @@
 import { env } from '@/env'
 
 export function api(path: string, init?: RequestInit) {
-  const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
+  // const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
+  const baseUrl = 'http://localhost:3000'
+
+  const apiPrefix = '/api'
 
   // O construtor URL() concatena uma baseURL com uma url
   // https://developer.mozilla.org/en-US/docs/Web/API/URL/URL
-  const url = new URL(path, baseUrl)
+  const url = new URL(apiPrefix.concat(path), baseUrl)
 
   return fetch(url, init)
 }
--- src/env.ts
+++ src/env.ts
@@ -1,22 +1,18 @@
-// parse/validação das variáveis de ambiente
 import { z } from 'zod'
 
 const envSchema = z.object({
   NEXT_PUBLIC_API_BASE_URL: z.string().url(),
 })
 
-// Pegando o parâmetro(process.env) e realizando a validação com o schema
 const parsedEnv = envSchema.safeParse(process.env)
 
 if (!parsedEnv.success) {
-  // flatten deixa mais legível os errors - https://zod.dev/ERROR_HANDLING?id=flattening-errors
   console.error(
-    'Invalid enviornment variables',
+    'Invalid environment variables',
     parsedEnv.error.flatten().fieldErrors,
   )
 
-  // Disparando um erro
-  throw new Error('Invalid environment variables')
+  throw new Error('Invalid environment variables.')
 }
 
 export const env = parsedEnv.data
