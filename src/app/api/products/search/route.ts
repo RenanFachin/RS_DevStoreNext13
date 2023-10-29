2030f61cbd68140e5a835f98d57cb09986585d43
Renan <renan.fachin@hotmail.com>
2023-10-29 18:16:30 -0300

    feat: Cria loading de busca para a página de search

--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -4,6 +4,8 @@ import type { NextRequest } from 'next/server'
 import data from '../data.json'
 
 export async function GET(request: NextRequest) {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
   const { searchParams } = request.nextUrl
 
   const query = z.string().parse(searchParams.get('q'))
